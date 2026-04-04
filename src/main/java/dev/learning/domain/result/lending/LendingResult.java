f1b26c8dd57745a3a01c2b44886b357e4f417cde
kiquetal <kiquetal@gmail.com>
2026-04-04 18:47:05 -0300

    feat: add MemberHasOverdueBooks record to LendingResult interface

--- src/main/java/dev/learning/domain/result/lending/LendingResult.java
+++ src/main/java/dev/learning/domain/result/lending/LendingResult.java
@@ -3,11 +3,13 @@ package dev.learning.domain.result.lending;
 import dev.learning.domain.type.MemberId;
 import dev.learning.domain.type.lending.BookLendingResult;
 
+import java.util.List;
+
 public sealed interface LendingResult {
     record Success(BookLendingResult book) implements LendingResult {}
     record AlreadyLent(BookLendingResult bookLending ) implements LendingResult {}
     record MemberNotFound(MemberId memberId) implements LendingResult{}
     record BookNotFound(String bookId) implements LendingResult{}
-    record MemberInDefault(MemberId memberId) implements LendingResult{}
+    record MemberHasOverdueBooks(MemberId memberId, List<BookLendingResult> books) implements LendingResult{}
     record MaximumLimitReached(MemberId memberId) implements LendingResult {}
 }
