668646663de7b3bad8f2a7b521314ecd897be3cf
kiquetal <kiquetal@gmail.com>
2026-04-04 18:30:18 -0300

    refactor: update BookLending entity to use BookItem and change date fields to Instant

--- src/main/java/dev/learning/domain/entity/BookLending.java
+++ src/main/java/dev/learning/domain/entity/BookLending.java
@@ -2,31 +2,30 @@ package dev.learning.domain.entity;
 
 import dev.learning.domain.LendingStatus;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "book_lending")
 public class BookLending extends PanacheEntity {
     @ManyToOne(optional = false)
-    public Book book;
+    @JoinColumn(name = "book_item_id", nullable = false)
+    public BookItem bookItem;
 
     @ManyToOne(optional = false)
+
     public Member member;
 
     @Column(name = "borrowed_at", nullable = false)
-    public LocalDate borrowedAt;
+    public Instant borrowedAt;
 
     @Column(name = "due_date", nullable = false)
-    public LocalDate dueDate;
+    public Instant dueDate;
 
     @Column(name = "returned_at")
-    public LocalDate returnedAt;
+    public Instant returnedAt;
 
     @Column(nullable = false, length = 20)
     @Enumerated(EnumType.STRING)
