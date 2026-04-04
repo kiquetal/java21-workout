be3682bcd57dd6ece1f8e336e44ada95a8ca4b6a
kiquetal <kiquetal@gmail.com>
2026-04-04 18:41:36 -0300

    feat: enhance BookItem and Member entities with additional fields and annotations

--- src/main/java/dev/learning/domain/entity/BookItem.java
+++ src/main/java/dev/learning/domain/entity/BookItem.java
@@ -1,10 +1,7 @@
 package dev.learning.domain.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class BookItem extends PanacheEntity
@@ -14,8 +11,7 @@ public class BookItem extends PanacheEntity
     Book book;
     String isbn;
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     BookItemStatus status;
-
-}
 
 }
--- src/main/java/dev/learning/domain/entity/Member.java
+++ src/main/java/dev/learning/domain/entity/Member.java
@@ -3,12 +3,16 @@ package dev.learning.domain.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.Set;
 
 @Entity
 public class Member extends PanacheEntity {
     @Column(nullable = false)
     public String name;
-
     @Column(unique = true, nullable = false)
     public String email;
+    @Column(name="address")
+    public String address;
 }
