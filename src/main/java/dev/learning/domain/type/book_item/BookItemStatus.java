2c147a8e8e248c46315d94bed7c416e966238b73
kiquetal <kiquetal@gmail.com>
2026-04-04 23:18:01 -0300

    feat: add BookItemRepository and BookItemStatus for managing book item availability

---
+++ src/main/java/dev/learning/domain/type/book_item/BookItemStatus.java
@@ -0,0 +1,8 @@
+package dev.learning.domain.type.book_item;
+
+public enum BookItemStatus
+{
+    AVAILABLE,
+    UNAVAILABLE,
+    DAMAGED
+}
---
+++ src/main/java/dev/learning/repository/BookItemRepository.java
@@ -0,0 +1,21 @@
+package dev.learning.repository;
+
+import dev.learning.domain.entity.BookItem;
+import dev.learning.domain.type.book_item.BookItemStatus;
+import dev.learning.domain.type.books.BookId;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class BookItemRepository implements PanacheRepository<BookItem>
+{
+
+    public boolean isAvailable(BookId  bookId) {
+
+        return find("book.id = ?1 and status = ?2", bookId.value(), BookItemStatus.AVAILABLE).firstResultOptional().isPresent();
+    }
+
+
+
+
+}
