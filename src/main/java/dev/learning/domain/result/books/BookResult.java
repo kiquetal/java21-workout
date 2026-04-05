6fc020bc0d6f91c8b17f41b5a706bb5f16a4eca9
kiquetal <kiquetal@gmail.com>
2026-04-04 23:28:47 -0300

    feat: simplify BookResult by removing BookId field

--- src/main/java/dev/learning/domain/result/books/BookResult.java
+++ src/main/java/dev/learning/domain/result/books/BookResult.java
@@ -1,7 +1,4 @@
 package dev.learning.domain.result.books;
-
-import dev.learning.domain.type.book_item.BookId;
-
-public record BookResult(BookId bookId, String isbn, String title, String author, int publicationYear)
+public record BookResult(String isbn, String title, String author, int publicationYear)
 {
 }
