6e2054b69646f6314b97db20129c7feaaf6a0c95
kiquetal <kiquetal@gmail.com>
2026-06-12 20:06:36 -0300

    feat: add LendingDetail record to represent lending information

--- src/main/java/dev/learning/domain/type/lending/LendingDetail.java
+++ src/main/java/dev/learning/domain/type/lending/LendingDetail.java
@@ -5,4 +5,4 @@ import dev.learning.domain.type.book_item.BookId;
 
 import java.time.Instant;
 
-public record BookLendingResult(BookId bookId, MemberId memberId, Instant dueTime, Instant borrowAt) {}
+public record LendingDetail(BookId bookId, MemberId memberId, Instant dueTime, Instant borrowAt) {}
