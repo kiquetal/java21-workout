e76595c88de442b950533fff761162d8d546d5aa
kiquetal <kiquetal@gmail.com>
2026-06-12 20:06:45 -0300

    refactor: update LendingResult records to use LendingDetail for book lending information

--- src/main/java/dev/learning/domain/result/lending/LendingResult.java
+++ src/main/java/dev/learning/domain/result/lending/LendingResult.java
@@ -1,15 +1,15 @@
 package dev.learning.domain.result.lending;
 
 import dev.learning.domain.type.member.MemberId;
-import dev.learning.domain.type.lending.BookLendingResult;
+import dev.learning.domain.type.lending.LendingDetail;
 
 import java.util.List;
 
 public sealed interface LendingResult {
-    record Success(BookLendingResult book) implements LendingResult {}
-    record AlreadyLent(BookLendingResult bookLending ) implements LendingResult {}
+    record Success(LendingDetail book) implements LendingResult {}
+    record AlreadyLent(LendingDetail bookLending ) implements LendingResult {}
     record MemberNotFound(MemberId memberId) implements LendingResult{}
     record BookNotFound(String bookId) implements LendingResult{}
-    record MemberHasOverdueBooks(MemberId memberId, List<BookLendingResult> books) implements LendingResult{}
+    record MemberHasOverdueBooks(MemberId memberId, List<LendingDetail> books) implements LendingResult{}
     record MaximumLimitReached(MemberId memberId) implements LendingResult {}
 }
