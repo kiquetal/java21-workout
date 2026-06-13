81bbeb2c9502bc2c333c42eccb6a6098f816966c
kiquetal <kiquetal@gmail.com>
2026-06-12 22:27:24 -0300

    refactor: update BookId to BookItemId in BookInfo and BookItemResult for consistency

--- src/main/java/dev/learning/domain/result/book_item/BookItemResult.java
+++ src/main/java/dev/learning/domain/result/book_item/BookItemResult.java
@@ -1,12 +1,12 @@
 package dev.learning.domain.result.book_item;
 
 import dev.learning.domain.type.book_item.BookItemInfo;
-import dev.learning.domain.type.book_item.BookId;
+import dev.learning.domain.type.book_item.BookItemId;
 
 public sealed interface BookItemResult {
     record BookItemAdded(BookItemInfo bookItemInfo) implements BookItemResult {}
     record BookItemRemoved(BookItemInfo bookItemInfo) implements BookItemResult{}
     record BookItemUpdated(BookItemInfo bookItemInfo) implements BookItemResult {}
-    record BookUnavailable(BookId bookId, String reason) implements BookItemResult {}
+    record BookUnavailable(BookItemId bookId, String reason) implements BookItemResult {}
 
 }
--- src/main/java/dev/learning/domain/type/books/BookInfo.java
+++ src/main/java/dev/learning/domain/type/books/BookInfo.java
@@ -1,5 +1,5 @@
 package dev.learning.domain.type.books;
 
-import dev.learning.domain.type.book_item.BookId;
+import dev.learning.domain.type.book_item.BookItemId;
 
-public record BookInfo(Isbn isbn, BookId bookId) {}
+public record BookInfo(Isbn isbn, BookItemId bookId) {}
