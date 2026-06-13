62e51f8ede91d4e52c949c0ed18b5e245e34f370
kiquetal <kiquetal@gmail.com>
2026-06-12 22:21:36 -0300

    feat: add checkOverdue method to LendingService for overdue book handling

--- src/main/java/dev/learning/service/LendingService.java
+++ src/main/java/dev/learning/service/LendingService.java
@@ -5,13 +5,19 @@ import dev.learning.domain.command.LendCommand;
 import dev.learning.domain.entity.Member;
 import dev.learning.domain.result.lending.LendingResult;
 import dev.learning.domain.type.Either;
+import dev.learning.domain.type.book_item.BookId;
+import dev.learning.domain.type.lending.LendingDetail;
+import dev.learning.domain.type.member.MemberId;
 import dev.learning.repository.BookRepository;
 import dev.learning.repository.LendingRepository;
 import dev.learning.repository.MemberRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.util.List;
 import java.util.Optional;
+
+import static java.util.Arrays.stream;
 
 @ApplicationScoped
 public class LendingService
@@ -35,5 +41,21 @@ public class LendingService
                 Either::right
         ).orElse(Either.left(new LendingResult.MemberNotFound(lendCommand.memberId())));
 
+    }
+
+    private Either<LendingResult,Member> checkOverdue(Member member)
+    {
+        var memberId = new MemberId(member.id);
+        var hasOverdue = lendingRepository.hasOverdueBook(memberId);
+        if (hasOverdue) {
+            var overdueBooks = lendingRepository.listBookLendingBorrowed(memberId);
+            //convert overdueBooks to bookdetails
+            List<LendingDetail> lendingDetails = overdueBooks.stream().map(
+                     b ->  new LendingDetail(new BookId(b.id), memberId, b.dueDate,b.borrowedAt)
+            ).collect(java.util.stream.Collectors.toList());
+
+            return Either.left(new LendingResult.MemberHasOverdueBooks(memberId, lendingDetails));
+        }
+        return Either.right(member);
     }
 }
