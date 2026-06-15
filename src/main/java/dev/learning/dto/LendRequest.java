78d36e44268aede2955664f635d36804edecf11e
kiquetal <kiquetal@gmail.com>
2026-06-14 22:06:39 -0300

    refactor: remove dueDate from LendRequest for simplified lending requests

--- src/main/java/dev/learning/dto/LendRequest.java
+++ src/main/java/dev/learning/dto/LendRequest.java
@@ -1,10 +1,8 @@
 package dev.learning.dto;
 
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDate;
 
 public record LendRequest(
     @NotNull Long bookId,
-    @NotNull Long memberId,
-    @NotNull LocalDate dueDate
+    @NotNull Long memberId
 ) {}
