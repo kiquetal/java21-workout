a28faf2bf3e6f936b909df2f48ed65d18f97105e
kiquetal <kiquetal@gmail.com>
2026-06-14 23:15:28 -0300

    feat: enhance LendingResourceTest with mocked repositories and initial lend method test setup

--- src/test/java/dev/learning/resource/LendingResourceTest.java
+++ src/test/java/dev/learning/resource/LendingResourceTest.java
@@ -1,7 +1,13 @@
 package dev.learning.resource;
 
+import dev.learning.repository.BookLendingRepository;
+import dev.learning.repository.MemberRepository;
+import dev.learning.service.LendingService;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -10,7 +16,12 @@ import static org.hamcrest.Matchers.*;
 class LendingResourceTest {
 
 
-
+    @InjectMock
+    BookLendingRepository bookLendingRepository;
+    @Inject
+    LendingService lendingService;
+    @InjectMock
+    MemberRepository memberRepository;
 
     @Test
     void lendWithNonExistentMember_returns404() {
@@ -68,6 +79,13 @@ class LendingResourceTest {
     @Test
     void checkMaximunNumberOfLending() {
 
+    //the method to test is lend from BookLendingService
+
+        Mockito.when(memberRepository.findByMemberId(Mockito.any())).thenReturn(java.util.Optional.of(new dev.learning.domain.Member(){{
+            id = 1L;
+        }}));
+        lendingService.lend()
+    Mockito.when()
     }
 
 
