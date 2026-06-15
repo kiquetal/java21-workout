4c8573debf6960671db7e9262b0248cbd9b2b2f3
kiquetal <kiquetal@gmail.com>
2026-06-14 22:07:44 -0300

    refactor: simplify LendCommand by removing unnecessary fields

--- src/main/java/dev/learning/domain/command/LendCommand.java
+++ src/main/java/dev/learning/domain/command/LendCommand.java
@@ -1,10 +1,7 @@
 package dev.learning.domain.command;
 
 import dev.learning.domain.type.book_item.BookItemId;
-import dev.learning.domain.type.lending.LendStatus;
 import dev.learning.domain.type.member.MemberId;
 
-import java.time.Instant;
-
-public record LendCommand(BookItemId bookItemId, MemberId memberId, Instant startDate, Instant endDate, LendStatus status){}
+public record LendCommand(BookItemId bookItemId, MemberId memberId){}
 
